1b577efd4ced76aa1a8c619e919af4e088b027fe
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-06-05 18:18:46 -0300

    feat: capturar texto do input e renderizar dinamicamente na tela

---
+++ 11-react-vite/11-pegando-valor-input/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { UserInput } from './components/UserInput'
+
+function App() {
+
+
+  return (
+    <>
+      <UserInput></UserInput>
+    </>
+  )
+}
+
+export default App
---
+++ 11-react-vite/11-pegando-valor-input/src/components/UserInput.tsx
@@ -0,0 +1,30 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
+
+export const UserInput = () => {
+
+    const [typedText, setTypedText] = useState("");
+    const [displayedText, setdisplayedText] = useState("");
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setTypedText(event.target.value);
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setdisplayedText(typedText);
+    }
+
+    
+
+    return (
+        <>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Digite algo para aparecer na tela" onChange={handleChange} />
+                <button type="submit">Enviar</button>
+            </form>
+            <p>Texto enviado: {displayedText}</p>
+        </>
+    )
+}
+
