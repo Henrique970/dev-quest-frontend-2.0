c41b522edb1fe183245ae7cfae86a348bc67a61f
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-06-09 19:14:38 -0300

    feat: adiciona aprendizagem de estilização no React

---
+++ 11-react-vite/13-estizando-react/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { BotaoExterno } from './components/BotaoExterno'
+import { BotaoModule } from './components/BotaoModule'
+import { BotaoStyled } from './components/BotaoStyled'
+function App() {
+  return (
+    <>
+      <BotaoExterno />
+      <BotaoModule />
+      <BotaoStyled />
+    </>
+  )
+}
+export default App
