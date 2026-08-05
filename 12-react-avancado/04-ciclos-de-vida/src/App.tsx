41954ad01ee210a101c2e9c1bfec5c80c0d6bded
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-08-05 18:30:56 -0300

    feat: adiconana inicio do estudo sobre ciclo de vida no React

---
+++ 12-react-avancado/04-ciclos-de-vida/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { MostraInicialização } from './components/MostraInicializacao'
+
+function App() {
+
+  return (
+    <>
+      <MostraInicialização mensagem='Jesus is King!'/>
+    </>
+  )
+}
+
+export default App
---
+++ 12-react-avancado/04-ciclos-de-vida/src/components/MostraInicializacao/index.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+export const MostraInicialização = ({ mensagem }: {mensagem : string}) => {
+    const [estado, setEstadp] = useState('inicialização');
+    
+    useEffect(() => {
+        console.log('Executa o useEffect ao renderizar o componente')
+    }, []);
+
+    return (
+        <div>
+            <h2>Estado interno</h2>
+            <p>{estado}</p>
+
+            <h2>Props recebidas</h2>
+            <pre>{JSON.stringify({mensagem}, null, 2)}</pre>
+        </div>
+    );
+}
